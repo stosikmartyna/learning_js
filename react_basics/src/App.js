9c584f7791b1b7bcab7e64d8d2c0c6f245048484
stosikmartyna <stosik.martyna@gmail.com>
2020-06-03 10:21:44 +0200

    Use the useState() hook for state manipulation.

--- react_basics/src/App.js
+++ react_basics/src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Content} from './Content'
 
 export const App = () => {
   const appTextContent = 'World';
+  const [persons, setPersons] = useState(undefined);
 
+  const handlePersons = () => {
+    setPersons([
+        { name: 'Ala', age: 25 },
+        { name: 'Max', age: 18 },
+        { name: 'Matt', age: 37 },
+        { name: 'Greg', age: 43 },
+    ])
+}
+
+const handleResetPersons = () => {
+    setPersons([])
+}
   return (
+    <>
     <Content textContent={appTextContent} />
+    <button onClick={handlePersons}>Get names</button>
+            <button onClick={handleResetPersons}>Reset names</button>
+            {persons &&
+                <ul>
+                    {persons.map((person, i) => {
+                        return <li key={i}>{person.name}</li>
+                    })}
+                </ul>
+            }
+    </>
   );
 }
