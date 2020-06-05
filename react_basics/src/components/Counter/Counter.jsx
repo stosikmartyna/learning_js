5025398d9f2ee5c47a617e0c53a817ab4afab95f
stosikmartyna <stosik.martyna@gmail.com>
2020-06-05 20:04:15 +0200

    Add condition to decrement function.

--- react_basics/src/components/Counter/Counter.jsx
+++ react_basics/src/components/Counter/Counter.jsx
@@ -3,12 +3,25 @@ import React, { useState } from 'react';
 export const Counter = () => {
     const [counter, setCounter] = useState(0);
 
+    const increment = () => {
+        setCounter(counter + 1)
+    }
+
+    const decrement = () => {
+        if (counter > 0) {
+            setCounter(counter - 1)
+        } 
+
+        // lub
+        // counter > 0 && setCounter(counter - 1)
+        // counter > 0 ? setCounter(counter - 1) : null
+    }
+
     return (
         <div style={{marginTop:'3rem'}}>
-            <button onClick={() => setCounter(counter-1)}> - </button>
+            <button onClick={decrement}> - </button>
             <span> {counter} </span>
-            <button onClick={() => setCounter(counter+1)}> + </button>
+            <button onClick={increment}> + </button>
         </div>
     )
 }
-    
