dfa61e2bf5f3b4ade111a002a98d5073e8a414df
stosikmartyna <stosik.martyna@gmail.com>
2020-06-05 18:55:46 +0200

    Call setCounter function directly in onClick props.

--- react_basics/src/components/Counter/Counter.jsx
+++ react_basics/src/components/Counter/Counter.jsx
@@ -3,19 +3,11 @@ import React, { useState } from 'react';
 export const Counter = () => {
     const [counter, setCounter] = useState(0);
 
-    const increment = () => {
-        setCounter(counter + 1)
-    }
-
-    const decrement = () => {
-        setCounter(counter - 1)
-    }
-
     return (
         <div style={{marginTop:'3rem'}}>
-            <button onClick={decrement}> - </button>
+            <button onClick={() => setCounter(counter-1)}> - </button>
             <span> {counter} </span>
-            <button onClick={increment}> + </button>
+            <button onClick={() => setCounter(counter+1)}> + </button>
         </div>
     )
 }
