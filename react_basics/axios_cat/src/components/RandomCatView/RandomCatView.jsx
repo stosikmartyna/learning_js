1179bcfdbd80230b331c1e76cf0b88c3eb5864e4
stosikmartyna <stosik.martyna@gmail.com>
2020-07-24 17:18:01 +0200

    Fetch API data and display description of selected cat.

--- react_basics/axios_cat/src/components/RandomCatView/RandomCatView.jsx
+++ react_basics/axios_cat/src/components/RandomCatView/RandomCatView.jsx
@@ -6,6 +6,8 @@ import './randomCatView.css';
 export const RandomCatView = () => {
     const [catData, setCatData] = useState([]);
     const [isFetching, setIsFetching] = useState(false);
+    const [catBreeds, setCatBreeds] = useState([]);
+    const [inputValue, setInputValue] = useState('');
 
     const fetchCatData = async() => { 
         setIsFetching(true);
@@ -17,11 +19,27 @@ export const RandomCatView = () => {
         } finally {
             setIsFetching(false);
         }
+    }    
+
+    const fetchCatBreeds = async() => {
+        try {
+            const response = await axios.get('https://api.thecatapi.com/v1/breeds')
+            setCatBreeds(response.data)
+        } catch (error) {
+            console.error(error)
+        }
     }
 
     useEffect(() => {
         fetchCatData();
+        fetchCatBreeds();
     }, []);
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    }
+
+    const selectedCat = catBreeds.find(cat => cat.name === inputValue);
 
     return (
         <div className="main_container">
@@ -32,6 +50,15 @@ export const RandomCatView = () => {
                     return <img src={cat.url} alt="cat_image" key={cat.id} /> 
                 })
             }
+            <label>Cat breeds</label>
+            <select name="cat_breeds" onChange={handleInputChange}>
+                <option>Choose breed</option>
+                {catBreeds.map(breed => {
+                    return <option value={breed.name} key={breed.id}>{breed.name}</option>
+                })}
+            </select>
+
+            <p>{selectedCat && selectedCat.description}</p>
         </div>
     )
 }
