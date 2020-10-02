ad8381870f3773ba9f8739c56b1f0a39d8744d39
stosikmartyna <stosik.martyna@gmail.com>
2020-10-02 19:46:55 +0200

    Store input values of add product section in component state

--- react_basics/dynamic_form/src/components/Form/Form.jsx
+++ react_basics/dynamic_form/src/components/Form/Form.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Form.css';
 
 export const Form = () => {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputValue = (event) => {
+        setInputValue({...inputValue, [event.target.id]: event.target.value});
+    }
+
     return (
         <>
             <h2>Dodaj produkt</h2>
             <div className="form"> 
-                <input type="text" placeholder="Produkt"/>
-                <input type="number" placeholder="Ilość"/>
-                <input type="number" placeholder="Cena netto"/>
-                <select name="vat" id="vat">
+                <input id="product" type="text" placeholder="Produkt" onChange={handleInputValue}/>
+                <input id="quantity" type="number" placeholder="Ilość" onChange={handleInputValue}/>
+                <input id="nettoPrice" type="number" placeholder="Cena netto" onChange={handleInputValue}/>
+                <select id="vat" name="vat" onChange={handleInputValue}>
                     <option value="">VAT</option>
                     <option value="vat8">8%</option>
                     <option value="vat23">23%</option>
